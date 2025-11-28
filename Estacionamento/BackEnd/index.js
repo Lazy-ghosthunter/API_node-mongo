6ffb48d831e217836d7b9bf1a1f7bafbc445744f
Ana <anakarolinesantana820@gmail.com>
2025-11-28 18:01:18 -0300

    controller proprietario e index

---
+++ Estacionamento/BackEnd/index.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8081;
+const proprietarioController = require('./controllers/ProprietarioControlls');
+
+//tratar o json
+app.use(bodyParser.json());
+
+//habilitar o cors
+app.use(cors());
+
+//rota teste
+app.get('/', (req, res) => 
+    res.send('Estou aqui')
+);
+
+//rota do controller
+app.unsubscribe('/proprietario', proprietarioController);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`));
+
